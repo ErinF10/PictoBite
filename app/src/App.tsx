eaa4dc9d772fe8d44ff9aafd497a943a9fb9c160
Hafeefa <hafeefa40@gmail.com>
2025-03-08 23:07:40 -0500

    added nutritional calculator

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
 import Api from './services/api'
+import PersonalizedNutrition from './services/PersonalizedNutrition'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Api />
+      <PersonalizedNutrition />
     </>
   )
 }
